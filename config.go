3556ce9ba732cc516cd5d50e48a3289e736d4dac
John Leach <john@brightbox.co.uk>
2011-10-13 00:31:23 +0100

    Config improvements
    * Create config path if it doesn't exist
    * Intelligently choose which API client in config to use
    * Report problems reading values from config

--- config.go
+++ config.go
@@ -4,16 +4,16 @@ import (
 	"brightbox"
 	"fmt"
 	"os"
-	"log"
 	"io/ioutil"
 	config "github.com/kless/goconfig/config"
 )
 
 var (
-  ErrConfigUnreadable  = os.NewError("brightbox: couldn't read config file")
+  ErrConfigUnreadable         = os.NewError("brightbox: couldn't read config file")
 )
 
 type Config struct {
+	path         string
 	filename     string
 	config       *config.Config
 	auth_url     string
@@ -27,23 +27,69 @@ type Config struct {
 func NewConfig() (*Config, os.Error) {
 	var	err   os.Error
 	conf := new(Config)
-	conf.filename = os.Getenv("HOME") + "/.brightbox/config"
+
+	err = conf.EnsurePath()
+	if err != nil {
+		return nil, err
+	}
+
+	conf.filename = conf.path + "/" + "config"
 	conf.config, err = config.ReadDefault(conf.filename)
+
 	if err != nil {
 		return nil, ErrConfigUnreadable
 	}
 	client_name := os.Getenv("CLIENT")
 	if client_name == "" {
-		log.Fatal("You must specify the config section name in environment variable CLIENT")
+		if len(conf.config.Sections()) == 2 {	// built in "default" section + one configured api client
+			client_name = conf.config.Sections()[1]
+		} else if len(conf.config.Sections()) > 2 {
+			client_name, err = conf.config.String("core", "default_client")
+		}
 	}
-	conf.auth_url, _ = conf.config.String(client_name, "auth_url")
-	conf.api_url, _ = conf.config.String(client_name, "api_url")
-	conf.client_id, _ = conf.config.String(client_name, "client_id")
-	conf.secret, _ = conf.config.String(client_name, "secret")
+
+	if client_name == "" {
+		return nil, os.NewError("Couldn't find an api client")
+	}
+	conf.api_url, err = conf.config.String(client_name, "api_url")
+	if err != nil {	return nil, err	}
+
+	conf.auth_url, err = conf.config.String(client_name, "auth_url")
 	if conf.auth_url == "" && conf.api_url != "" {
 		conf.auth_url = conf.api_url
 	}
+
+	conf.client_id, err = conf.config.String(client_name, "client_id")
+	if err != nil {	return nil, err	}
+
+	conf.secret, err = conf.config.String(client_name, "secret")
+	if err != nil {	return nil, err	}
+
 	return conf, nil
+}
+
+func (conf *Config) EnsurePath() os.Error {
+	var err os.Error
+
+	if conf.path != "" {
+		return nil
+	}
+	path := os.Getenv("HOME") + "/.brightbox"
+
+	stat, err := os.Stat(path)
+	if err != nil {
+		return os.NewError("Cannot create config path ("+path+"): " + err.String())
+	}
+
+	if stat.IsDirectory() == false {
+		err = os.Mkdir(path, 0700)
+		if err != nil {
+			return os.NewError("Cannot create config path ("+path+"): " + err.String())
+		}
+	}
+
+	conf.path = path
+	return nil
 }
 
 // SetupAuthenticatorCache tries to read a cached token from the local filesystem
@@ -54,7 +100,8 @@ func (conf *Config) SetupAuthenticatorCache(auth brightbox.Authenticator) os.Err
 		expires   int64
 		f         *os.File
 	)
-	cache_filename := "/home/john/.brightbox/" + auth.String() + ".oauth_token.v2"
+	conf.EnsurePath()
+	cache_filename := conf.path + "/" + auth.String() + ".oauth_token.v2"
 	f, err = os.Open(cache_filename)
 	if f != nil {
 		_, err = fmt.Fscanf(f, "%s", &token)
