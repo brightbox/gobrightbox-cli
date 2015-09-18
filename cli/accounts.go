306e5f79ccc7f2a7fcb76c81fdb520020fef933a
John Leach <john@johnleach.co.uk>
2015-09-19 00:01:27 +0100

    Remove some copy+paste messups

--- cli/accounts.go
+++ cli/accounts.go
@@ -30,55 +30,8 @@ func (l *AccountsCommand) list(pc *kingpin.ParseContext) error {
 	return nil
 }
 
-func (l *AccountsCommand) show(pc *kingpin.ParseContext) error {
-	err := l.App.Configure()
-	if err != nil {
-		return err
-	}
-	w := tabWriterRight()
-	defer w.Flush()
-	s, err := l.App.Client.Server(l.Id)
-	if err != nil {
-		l.App.Fatalf(err.Error())
-	}
-
-	drawShow(w, []interface{}{
-		"id", s.Id,
-		"status", s.Status,
-		"locked", s.Locked,
-		"name", s.Name,
-		"created_at", s.CreatedAt,
-		"deleted_at", s.DeletedAt,
-		"zone", s.Zone.Handle,
-		"type", s.ServerType.Id,
-		"type_name", s.ServerType.Name,
-		"type_handle", s.ServerType.Handle,
-		"ram", s.ServerType.Ram,
-		"cores", s.ServerType.Cores,
-		"disk", s.ServerType.DiskSize,
-		"compatability_mode", s.CompatabilityMode,
-		"image", s.Image.Id,
-		"image_name", s.Image.Name,
-		"arch", s.Image.Arch,
-		"private_ips", collectByField(s.Interfaces, "IPv4Address"),
-		"cloud_ips", collectByField(s.CloudIPs, "PublicIP"),
-		"ipv6_ips", collectByField(s.Interfaces, "IPv6Address"),
-		"cloud_ip_ids", collectByField(s.CloudIPs, "Id"),
-		"hostname", s.Hostname,
-		"fqdn", s.Fqdn,
-		"public_hostname", "public." + s.Fqdn,
-		"ipv6_hostname", "ipv6." + s.Fqdn,
-		"snapshots", collectById(s.Snapshots),
-		"server_groups", collectById(s.ServerGroups),
-	})
-	return nil
-
-}
-
 func ConfigureAccountsCommand(app *CliApp) {
 	cmd := AccountsCommand{App: app}
 	accounts := app.Command("accounts", "manage accounts")
 	accounts.Command("list", "list accounts").Action(cmd.list)
-	show := accounts.Command("show", "view details of an account").Action(cmd.show)
-	show.Arg("identifier", "identifier of server to show").Required().StringVar(&cmd.Id)
 }
--- cli/server_groups.go
+++ cli/server_groups.go
@@ -28,55 +28,8 @@ func (l *ServerGroupsCommand) list(pc *kingpin.ParseContext) error {
 	return nil
 }
 
-func (l *ServerGroupsCommand) show(pc *kingpin.ParseContext) error {
-	err := l.App.Configure()
-	if err != nil {
-		return err
-	}
-	w := tabWriterRight()
-	defer w.Flush()
-	s, err := l.App.Client.Server(l.Id)
-	if err != nil {
-		l.App.Fatalf(err.Error())
-	}
-
-	drawShow(w, []interface{}{
-		"id", s.Id,
-		"status", s.Status,
-		"locked", s.Locked,
-		"name", s.Name,
-		"created_at", s.CreatedAt,
-		"deleted_at", s.DeletedAt,
-		"zone", s.Zone.Handle,
-		"type", s.ServerType.Id,
-		"type_name", s.ServerType.Name,
-		"type_handle", s.ServerType.Handle,
-		"ram", s.ServerType.Ram,
-		"cores", s.ServerType.Cores,
-		"disk", s.ServerType.DiskSize,
-		"compatability_mode", s.CompatabilityMode,
-		"image", s.Image.Id,
-		"image_name", s.Image.Name,
-		"arch", s.Image.Arch,
-		"private_ips", collectByField(s.Interfaces, "IPv4Address"),
-		"cloud_ips", collectByField(s.CloudIPs, "PublicIP"),
-		"ipv6_ips", collectByField(s.Interfaces, "IPv6Address"),
-		"cloud_ip_ids", collectByField(s.CloudIPs, "Id"),
-		"hostname", s.Hostname,
-		"fqdn", s.Fqdn,
-		"public_hostname", "public." + s.Fqdn,
-		"ipv6_hostname", "ipv6." + s.Fqdn,
-		"snapshots", collectById(s.Snapshots),
-		"server_groups", collectById(s.ServerGroups),
-	})
-	return nil
-
-}
-
 func ConfigureServerGroupsCommand(app *CliApp) {
 	cmd := ServerGroupsCommand{App: app}
 	groups := app.Command("groups", "manage server groups")
 	groups.Command("list", "list server groups").Action(cmd.list)
-	show := groups.Command("show", "view details of a server group").Action(cmd.show)
-	show.Arg("identifier", "identifier of server group to show").Required().StringVar(&cmd.Id)
 }
