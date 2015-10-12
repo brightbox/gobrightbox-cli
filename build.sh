a85a8706dcbede8c4a645a036b64131826da5579
John Leach <john@johnleach.co.uk>
2015-10-12 23:04:03 +0100

    clean bin dir before build

--- build.sh
+++ build.sh
@@ -5,8 +5,10 @@ set -ex
 go get -v -t -d
 
 mkdir -p bin/
+rm -f bin/* || true
 
 export GOOS GOARCH
+
 for GOOS in darwin linux; do
 	for GOARCH in 386 amd64; do
 		go build -o bin/gobrightbox-$GOOS-$GOARCH
