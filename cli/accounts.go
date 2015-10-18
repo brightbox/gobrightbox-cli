ac8faaf4987b4d5a92b1e44c8ab343b618fe211e
John Leach <john@johnleach.co.uk>
2015-10-18 22:45:45 +0100

    Accounts role support
    Retrieve and display collaboraton roles in accounts list and show

--- cli/accounts.go
+++ cli/accounts.go
@@ -8,10 +8,15 @@ import (
 
 var (
 	DefaultAccountListFields = []string{"id", "status", "role", "ram_used", "lb_used", "dbs_ram_used", "name"}
-	DefaultAccountShowFields = []string{"id", "name", "status", "cloud_ips_limit",
+	DefaultAccountShowFields = []string{"id", "name", "status", "role", "cloud_ips_limit",
 		"cloud_ips_used", "ram_limit", "ram_used", "lb_limit", "lb_used",
 		"dbs_ram_limit", "dbs_ram_used", "library_ftp_host", "library_ftp_user"}
 )
+
+type Account struct {
+	*brightbox.Account
+	Role string
+}
 
 type AccountsCommand struct {
 	*CliApp
@@ -19,12 +24,12 @@ type AccountsCommand struct {
 	Fields string
 }
 
-func AccountFields(a *brightbox.Account) map[string]string {
+func AccountFields(a Account) map[string]string {
 	return map[string]string{
 		"id":                   a.Id,
 		"status":               a.Status,
 		"name":                 a.Name,
-		"role":                 "",
+		"role":                 a.Role,
 		"cloud_ips_limit":      formatInt(a.CloudIpsLimit),
 		"cloud_ips_used":       formatInt(a.CloudIpsUsed),
 		"ram_limit":            formatInt(a.RamLimit),
@@ -57,8 +62,24 @@ func (l *AccountsCommand) list(pc *kingpin.ParseContext) error {
 	if err != nil {
 		return err
 	}
+
+	colmap := make(map[string]string)
+	collabs, err := l.Client.Collaborations()
+	// An error here most likely just means that we're using a api client and
+	// not a user client. So if it succeeds, we're a user with
+	// collaborations. If it fails, we're an api client and don't have
+	// collaborations.
+	if err == nil {
+		for _, a := range *accounts {
+			colmap[a.Id] = "Owner"
+		}
+		for _, col := range *collabs {
+			colmap[col.Account.Id] = col.RoleLabel
+		}
+	}
+
 	for _, a := range *accounts {
-		if err = out.Write(AccountFields(&a)); err != nil {
+		if err = out.Write(AccountFields(Account{&a, colmap[a.Id]})); err != nil {
 			return err
 		}
 	}
@@ -78,7 +99,16 @@ func (l *AccountsCommand) show(pc *kingpin.ParseContext) error {
 	if err != nil {
 		return err
 	}
-	if err = out.Write(AccountFields(account)); err != nil {
+	collabs, err := l.Client.Collaborations()
+	colmap := make(map[string]string)
+	if err == nil {
+		colmap[account.Id] = "Owner"
+		for _, col := range *collabs {
+			colmap[col.Account.Id] = col.RoleLabel
+		}
+	}
+
+	if err = out.Write(AccountFields(Account{account, colmap[account.Id]})); err != nil {
 		return err
 	}
 	out.Flush()
